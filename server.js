9b3802a7a1e9847c227bc2d5bece340d54b9e0e3
myth19950803 <myth19950803@gmail.com>
2021-11-11 03:53:40 +0300

    get count of users table.

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const path = __dirname + '/app/views/';
+// const path = __dirname + '/app/views/';
 
 const app = express();
 
-app.use(express.static(path));
+// app.use(express.static(path));
 
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -22,20 +22,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 
-db.sequelize.sync();
+// db.sequelize.sync();
 // // drop the table if it already exists
 // db.sequelize.sync({ force: true }).then(() => {
 //   console.log("Drop and re-sync db.");
 // });
 
 app.get('/', function (req,res) {
-  res.sendFile(path + "index.html");
+  // res.sendFile(path + "index.html");
+  res.send("Testing...");
 });
 
-require("./app/routes/turorial.routes")(app);
+require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
